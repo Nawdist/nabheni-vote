37197ddaad05ea882c54786d54f6aca1b8cce1f6
nawdist <receptivegamer@gmail.com>
2022-04-30 18:49:42 +0100

    commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Site du vote pour le logo du lycée
+
+### Pour tester:
+
+```properties
+npm install
+npm run dev
+```  
+
+>Version NodeJS: v16.6+ [Installer ici si vous n'avez pas cette version.]([https://link](https://nodejs.org/en/download/))
+---
+
+⚠️ *puisqe les images ont été arbitrairement nommées, on doit les remplacer dans `./public/images` avec les images correspondantes*
+
+
+
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bp = require("body-parser")
 const dotenv = require("dotenv").config()
-const { MongoClient, ServerApiVersion } = require("mongodb")
-const EmailValidator = require("email-validator")
-
+const { MongoClient, ServerApiVersion, ConnectionClosedEvent } = require("mongodb")
+const NodeCache = require("node-cache")
+const cache = new NodeCache()
 
 const client = new MongoClient(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 const app = express();
@@ -13,8 +13,16 @@ app.use(express.static(__dirname + "/public"))
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html")
+
+let checkip = (req, res, next) => {
+    let ip = req.ip || req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+    console.log(ip)
+    if(cache.get(ip)) res.status(403).redirect("/voted")
+    else next()
+}
+
+app.get("/", checkip, (req, res) => {
+    res.sendFile(__dirname + "/public/home.html")
 })
 
 app.post("/submit", (req, res) => {
@@ -22,25 +30,33 @@ app.post("/submit", (req, res) => {
 
     res.setHeader("Content-Type", "application/json")
 
-    if (!EmailValidator.validate(q.email)) return res.send({ error: "Adresse e-mail invalide" })
     if (q.img == "undefined") return res.send({ error: "Pas d'image sélectionée" })
 
     client.connect().then(db => {
         const dbo = db.db()
 
-        dbo.collection("votes").insertOne({ email: q.email, logo: { id: q.img, auteur: q.auteur }, date: Date.now() })
+        dbo.collection("votes").insertOne({logo: { id: q.img, auteur: q.auteur }, date: Date.now() })
             .then(doc => {
-                res.send({ redirect: "/sucess" })
-
+                let ip = req.ip || req.headers['x-forwarded-for'] || req.socket.remoteAddress
+                res.cookie("voted", q.img, { maxAge: 1000 * 60 * 60 * 24 * 7 })
+                cache.set(ip, q.img, 0)
+                res.send({ redirect: "/voted" })
                 db.close()
 
             }).catch(err => console.log(err))
     })
-    .catch(err => console.log("error connecting", err))
+        .catch(err => console.log("error connecting", err))
 })
 
-app.get("/sucess", (req, res) => {
-    res.send("voted")
+app.get("/voted", (req, res) => {
+    console.log("yes")
+    res.sendFile(__dirname + "/public/voted.html")
 })
+
+function checkCookie(req, res, next) {
+    console.log("h")
+    console.log(req?.headers)
+    next()
+}
 
 app.listen(PORT, () => console.log(`Listening on port:${PORT}`))
